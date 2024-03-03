53a77eb31d82d8734a877883bcae037492c6a6e9
Paulo <pauloeduardo_almeida@hotmail.com>
2024-03-03 14:20:07 -0300

    fix: archupdate notify

--- .config/fish/functions/findfiles.fish
+++ .config/fish/functions/findfiles.fish
@@ -1,6 +1,6 @@
 function findfiles
 	set -f dir $HOME
-	set -f search (fd . "$dir" -H | fzf --prompt "$dir " --header "CTRL-d: Dir | CTRL-f: Files" --bind "ctrl-d:change-prompt(Dir > )+reload(fd . $dir -t d -H)" --bind "ctrl-f:change-prompt(Files > )+reload(fd . $dir -t f -H)")
+	set -f search (fd . "$dir" -H | fzf --prompt "$dir " --header "CTRL-d: Dir | CTRL-f: Files | CTRL-s: System" --bind "ctrl-d:change-prompt(Dir > )+reload(fd . $dir -t d -H)" --bind "ctrl-f:change-prompt(Files > )+reload(fd . $dir -t f -H)" --bind "ctrl-s:change-prompt(System > )+reload(fd . / -H)")
 
 	if test -d "$search"
 		cd "$search"
